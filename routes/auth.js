6f9fb24b5693b1e784fb6808348f102256fb5249
MatsuoCN <140224764+MatsuoCN@users.noreply.github.com>
2026-08-13 16:18:45 -0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
+const jwt = require('jsonwebtoken');
 
 // Rota de registro de usuário
 router.post('/register', async (req, res) => {
@@ -27,6 +28,34 @@ router.post('/register', async (req, res) => {
     console.error('Erro ao registrar usuário:', erro);
     res.status(500).json({ message: 'Erro ao registrar usuário' });
 
+  }
+});
+
+// Rota de Login de usuário
+router.post('/login', async (req, res) => {
+  try {
+    const { email, senha } = req.body;
+
+    // 1. Verifica se o usuário existe no banco
+    const usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+      return res.status(400).json({ message: 'Email ou senha incorretos' });
+    }
+
+    // 2. Compara a senha digitada com a senha embaralhada do banco
+    const senhaValida = await bcrypt.compare(senha, usuario.senha);
+    if (!senhaValida) {
+      return res.status(400).json({ message: 'Email ou senha incorretos' });
+    }
+
+    // 3. Gera o crachá de acesso (Token) que dura 1 hora
+    const token = jwt.sign({ id: usuario._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ message: 'Login realizado com sucesso', token });
+
+  } catch (erro) {
+    console.error('Erro no login:', erro);
+    res.status(500).json({ message: 'Erro ao fazer login' });
   }
 });
 
